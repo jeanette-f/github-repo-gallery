257e3c1f6674c4f1c0a93ba4d2fdbcee4db0d619
j_ferlan <jeanette.e.carroll@gmail.com>
2024-02-21 12:55:14 -0800

    Add API to fetch repo information

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const overview = document.querySelector (".overview");
 const username = "jeanette-f";
+const repoList = document.querySelector (".repo-list");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch (`https://api.github.com/users/${username}`);
@@ -25,4 +26,21 @@ const displayUserInfo = function(data) {
     </div> 
     `;
     overview.append(div);
+    gitRepos();
+};
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=15`);
+    const repoData = await fetchRepos.json();
+    // console.log (repoData);
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement ("li");
+        repoItem.classList.add ("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
